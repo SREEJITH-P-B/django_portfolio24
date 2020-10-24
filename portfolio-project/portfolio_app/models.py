246f7e0f1d9a79fb1975212d5e8861d5791a29e5
SREEJITH-P-B <33229709+SREEJITH-P-B@users.noreply.github.com>
2020-10-24 09:30:44 +0000

    completed project portfolio app

--- portfolio-project/portfolio_app/models.py
+++ portfolio-project/portfolio_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+class Works(models.Model):
+    image=models.ImageField(upload_to='images/')
+    summary=models.CharField(max_length=200)
+    
 
-# Create your models here.
--- portfolio-project/portfolio_app/views.py
+++ portfolio-project/portfolio_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Works
 def index(request):
-    return render(request,'portfolio_app/index.html')
+    works=Works.objects
+    return render(request,'portfolio_app/index.html',{'works':works})
     
 # Create your views here.
